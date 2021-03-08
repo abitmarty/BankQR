78f422110f2560fa1bb84e9206628a2ef6b843eb
abitmarty <matijn.totte@gmail.com>
2021-03-08 21:08:07 +0100

    Rederect home after scan

--- components/QRScanner/index.js
+++ components/QRScanner/index.js
@@ -2,13 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button, Vibration } from 'react-native';
 import styles from './styles';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import { useNavigation } from '@react-navigation/native';
 
 
 const QRScanner = (props) => {
     const [hasPermission, setHasPermission] = useState(null);
     const [scanned, setScanned] = useState(false);
+    const navigation = useNavigation(); 
 
     const ONE_SECOND_IN_MS = 1000;
+
+    
 
     useEffect(() => {
         (async () => {
@@ -17,18 +21,19 @@ const QRScanner = (props) => {
         })();
       }, []);
     
-      const handleBarCodeScanned = ({ type, data }) => {
-        setScanned(true);
-        Vibration.vibrate(1 * ONE_SECOND_IN_MS)
-        alert(`Payment of €${data} has been payed!`);
-      };
-    
-      if (hasPermission === null) {
-        return <Text>Requesting for camera permission</Text>;
-      }
-      if (hasPermission === false) {
-        return <Text>No access to camera</Text>;
-      }
+    const handleBarCodeScanned = ({ type, data }) => {
+      setScanned(true);
+      Vibration.vibrate(1 * ONE_SECOND_IN_MS)
+      alert(`Payment of €${data} successful!`);
+      navigation.replace('Root')
+    };
+  
+    if (hasPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
     
     return (
         <View style={styles.container}>
