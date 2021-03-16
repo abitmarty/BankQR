b94d11afabcd3e1e4b3c989e5b3c45f081bc1825
abitmarty <matijn.totte@gmail.com>
2021-03-16 14:38:19 +0100

    Added close keyboard button

--- components/ScannerRef/index.js
+++ components/ScannerRef/index.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { TouchableOpacity, Alert, Text, View, StyleSheet, Button, Vibration } from 'react-native';
+import { Keyboard, TouchableOpacity, Alert, Text, View, StyleSheet, Button, Vibration } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';  
 import { useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons'; 
 
 
 const ScannerRef = (props) => {
   const navigation = useNavigation(); 
     return (
         <View>
-        <TouchableOpacity onPress={() => navigation.navigate('Scanner')} >
-          <Ionicons name="scan-outline" size={24} color="white" />
+        <TouchableOpacity onPress={() => Keyboard.dismiss()} >
+          <FontAwesome name="close" size={24} color="white" />
         </TouchableOpacity>
         </View>
     )
