0dd99b555fabcd94662fe688a3ffaadee09763c1
abitmarty <matijn.totte@gmail.com>
2021-03-16 17:37:50 +0100

    Removed popup

--- components/QRScanner/index.js
+++ components/QRScanner/index.js
@@ -124,7 +124,6 @@ const QRScanner = (props) => {
                 onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
                 style={[StyleSheet.absoluteFillObject]}
             />
-            {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
         </View>
     )
 }
